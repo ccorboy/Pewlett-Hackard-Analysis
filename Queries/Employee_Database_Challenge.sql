5ffcd7a35c3709b8ec9ef6dcc5c0042cf05658db
ccorboy <77373603+ccorboy@users.noreply.github.com>
2021-03-13 16:48:43 -0800

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,1 +1,30 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
 
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
+
+-- Deliverable 1 part 2: distinct clause to remove duplicate employees
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) rt.emp_no,
+				   rt.first_name,
+				   rt.last_name,
+				   rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_titles;
+
+--Deliverable 1 part 3: retiree count for each department
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.emp_no) DESC;
+
+SELECT * FROM retiring_titles;
