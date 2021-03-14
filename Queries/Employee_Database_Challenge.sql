37df6266c727302724b03ce735b6a9971d57724f
ccorboy <77373603+ccorboy@users.noreply.github.com>
2021-03-13 16:49:45 -0800

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -8,6 +8,22 @@ INTO nameyourtable
 FROM _______
 ORDER BY _____, _____ DESC;
 
+-- Deliverable 1: # of retiring emps by title
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   ti.title,
+	   ti.from_date,
+	   ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
 -- Deliverable 1 part 2: distinct clause to remove duplicate employees
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) rt.emp_no,
